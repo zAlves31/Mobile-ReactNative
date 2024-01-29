cbe13c90c1b3aa4d21608ebb076be49dce741ec7
zAlves31 <alvesbautistajoaovictor@gmail.com>
2024-01-29 10:58:13 -0300

    revisao

--- revisaoProps/revisaoprops/App.js
+++ revisaoProps/revisaoprops/App.js
@@ -1,16 +1,54 @@
-import { SafeAreaView, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, ScrollView, FlatList } from 'react-native';
 import Person from './src/componentes/Person/Person';
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Person name='Joao' age='19'/>
-      <Person name='Eduardo' age='18'/>
-      <Person name='Gustavo' age='18'/>
-      <Person name='Gab' age='16'/>
 
-      <StatusBar/>  
-    </SafeAreaView>
+  //simulando uma lista de pessoas
+  const peoplelist = [
+    {id: '1', name:'joao', age:19},
+    {id: '2', name:'Gab', age:16},
+    {id: '3', name:'Eduardo', age:18},
+    {id: '4', name:'Gustavo', age:18}
+
+  ]
+
+  return (
+    // <ScrollView>
+    //   <SafeAreaView>
+    //     <Person name='Joao' age='19'/>
+    //     <Person name='Eduardo' age='18'/>
+    //     <Person name='Gustavo' age='18'/>
+    //     <Person name='Gab' age='16'/>
+    //     <Person name='Vicinius' age='17'/>
+    //     <Person name='Crystal' age='13'/>
+    //     <Person name='celsao' age='56'/>
+    //     <Person name='Silvio Santos' age='93'/>
+    //     <Person name='Gabriel Victor' age='17'/>
+    //     <Person name='Marcio' age='29'/>
+    //     <Person name='Marcelo' age='19'/>
+    //     <Person name='Gru' age='35'/>
+    //     <Person name='Macedo' age='17'/>
+    //     <Person name='Enzo' age='16'/>
+    //     <Person name='Eduardo' age='14'/>
+    //     <Person name='Alan' age='33'/>
+    //     <Person name='Everton' age='??'/>
+
+    //     <StatusBar/>  
+    //   </SafeAreaView>
+    // </ScrollView>
+
+    // usando FlatList
+    <FlatList
+      data={peoplelist}
+      keyExtractor={(item) => item.id}
+
+      //leitura da lista
+      renderItem={({item}) =>(
+
+        //exibe cada item da lista
+        <Person name={item.name} age={item.age}/>
+      )}
+    />
   );
 }
 
--- revisaoProps/revisaoprops/src/componentes/Person/Person.js
+++ revisaoProps/revisaoprops/src/componentes/Person/Person.js
@@ -1,23 +1,43 @@
 import {StyleSheet, Text, View} from "react-native";
+import { useFonts, Inconsolata_600SemiBold  } from '@expo-google-fonts/inconsolata';
+
 
 //componente Person
 //props : name, age
 const Person = ({name, age}) => {
+    let [fontsLoaded, fontError] = useFonts({
+        Inconsolata_600SemiBold,
+      });
+    
+      if (!fontsLoaded && !fontError) {
+        return null;
+      }
+      
     return (
+        
         <View style={styles.container}>
-            <Text>Nome: {name}</Text>
-            <Text>Idade: {age}</Text>
+            <Text style={styles.txt}>Nome: {name}</Text>
+            <Text style={styles.txt}>Idade: {age}</Text>
         </View>
     );
 }
     const styles = StyleSheet.create({
         container: {
-            backgroundColor: '#EB80BB',
+            backgroundColor: '#FAB4F0',
             padding:10,
             margin:10,
-            borderRadius:5
+            borderRadius:5,
+            borderWidth:3,
+            borderColor:"black"
+            
+        },
+
+        txt: {
+            fontFamily:"Inconsolata_600SemiBold",
+            fontSize:15
         }
     })
 
 
 export default Person;
+
